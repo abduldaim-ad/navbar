025c209f22caebe3be4b738031a35655c75273de
ad551 <gdaim551@gmail.com>
2022-03-15 20:03:03 +0500

    earliest commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
+import { Outlet, Link } from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+<div>
+      <h1>Navbar</h1>
+      <nav
+        style={{
+          borderBottom: "solid 1px",
+          paddingBottom: "1rem",
+        }}
+      >
+        <Link to="/firstlink">Link_A</Link> |{" "}
+        <Link to="/secondlink">Link_B</Link>
+      </nav>
+      <Outlet />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { render } from "react-dom";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import App from "./App";
+import Expenses from "./routes/expenses";
+import Invoices from "./routes/invoices";
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+<BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route path="firstlink" element={<Expenses />} />
+        <Route path="secondlink" element={<Invoices />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
